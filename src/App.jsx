9555c0f288aac6f17e814eaed6f5517996400ac1
Vitya Machine <vatamanenko9@gmail.com>
2023-04-06 22:07:25 +0300

    react 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import Contacts, { Container } from './components/Container/Container';
+import Profile from './components/Profile/Profile';
+
+import user from './data/user.json';
+
 function App() {
-  return <div className="App">App App</div>;
+  return (
+    <>
+      <Container bgColor={'#C0DBEA'}>
+        <Profile
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Container>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import styles from './Container.module.css';
+
+export function Container({ bgColor, children }) {
+  return (
+    <div
+      className={styles.container}
+      style={{
+        backgroundColor: bgColor,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
